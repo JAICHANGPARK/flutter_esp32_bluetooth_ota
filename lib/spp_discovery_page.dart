691c8e270f3a21244279d490af8d2e0c04f06731
JAICHANG.PARK <jaichang@angel-robotics.com>
2020-09-02 08:31:33 +0900

    Create spp_discovery_page.dart

---
+++ lib/spp_discovery_page.dart
@@ -0,0 +1,223 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
+
+class DiscoveryPage extends StatefulWidget {
+  /// If true, discovery starts on page start, otherwise user must press action button.
+  final bool start;
+
+  const DiscoveryPage({this.start = true});
+
+  @override
+  _DiscoveryPage createState() => new _DiscoveryPage();
+}
+
+class _DiscoveryPage extends State<DiscoveryPage> {
+  StreamSubscription<BluetoothDiscoveryResult> _streamSubscription;
+  List<BluetoothDiscoveryResult> results = List<BluetoothDiscoveryResult>();
+  bool isDiscovering;
+
+  _DiscoveryPage();
+
+  @override
+  void initState() {
+    super.initState();
+
+    isDiscovering = widget.start;
+    if (isDiscovering) {
+      _startDiscovery();
+    }
+  }
+
+  void _restartDiscovery() {
+    setState(() {
+      results.clear();
+      isDiscovering = true;
+    });
+
+    _startDiscovery();
+  }
+
+  void _startDiscovery() {
+    _streamSubscription =
+        FlutterBluetoothSerial.instance.startDiscovery().listen((r) {
+          setState(() {
+            results.add(r);
+          });
+        });
+
+    _streamSubscription.onDone(() {
+      setState(() {
+        isDiscovering = false;
+      });
+    });
+  }
+
+  // @TODO . One day there should be `_pairDevice` on long tap on something... ;)
+
+  @override
+  void dispose() {
+    // Avoid memory leak (`setState` after dispose) and cancel discovery
+    _streamSubscription?.cancel();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: isDiscovering
+            ? Text('Discovering devices')
+            : Text('Discovered devices'),
+        actions: <Widget>[
+          isDiscovering
+              ? FittedBox(
+            child: Container(
+              margin: new EdgeInsets.all(16.0),
+              child: CircularProgressIndicator(
+                valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+              ),
+            ),
+          )
+              : IconButton(
+            icon: Icon(Icons.replay),
+            onPressed: _restartDiscovery,
+          )
+        ],
+      ),
+      body: ListView.builder(
+        itemCount: results.length,
+        itemBuilder: (BuildContext context, index) {
+          BluetoothDiscoveryResult result = results[index];
+          return BluetoothDeviceListEntry(
+            device: result.device,
+            rssi: result.rssi,
+            onTap: () {
+              Navigator.of(context).pop(result.device);
+            },
+            onLongPress: () async {
+              try {
+                bool bonded = false;
+                if (result.device.isBonded) {
+                  print('Unbonding from ${result.device.address}...');
+                  await FlutterBluetoothSerial.instance
+                      .removeDeviceBondWithAddress(result.device.address);
+                  print('Unbonding from ${result.device.address} has succed');
+                } else {
+                  print('Bonding with ${result.device.address}...');
+                  bonded = await FlutterBluetoothSerial.instance
+                      .bondDeviceAtAddress(result.device.address);
+                  print(
+                      'Bonding with ${result.device.address} has ${bonded ? 'succed' : 'failed'}.');
+                }
+                setState(() {
+                  results[results.indexOf(result)] = BluetoothDiscoveryResult(
+                      device: BluetoothDevice(
+                        name: result.device.name ?? '',
+                        address: result.device.address,
+                        type: result.device.type,
+                        bondState: bonded
+                            ? BluetoothBondState.bonded
+                            : BluetoothBondState.none,
+                      ),
+                      rssi: result.rssi);
+                });
+              } catch (ex) {
+                showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      title: const Text('Error occured while bonding'),
+                      content: Text("${ex.toString()}"),
+                      actions: <Widget>[
+                        new FlatButton(
+                          child: new Text("Close"),
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                        ),
+                      ],
+                    );
+                  },
+                );
+              }
+            },
+          );
+        },
+      ),
+    );
+  }
+}
+
+
+class BluetoothDeviceListEntry extends ListTile {
+  BluetoothDeviceListEntry({
+    @required BluetoothDevice device,
+    int rssi,
+    GestureTapCallback onTap,
+    GestureLongPressCallback onLongPress,
+    bool enabled = true,
+  }) : super(
+    onTap: onTap,
+    onLongPress: onLongPress,
+    enabled: enabled,
+    leading:
+    Icon(Icons.devices), // @TODO . !BluetoothClass! class aware icon
+    title: Text(device.name ?? "Unknown device"),
+    subtitle: Text(device.address.toString()),
+    trailing: Row(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        rssi != null
+            ? Container(
+          margin: new EdgeInsets.all(8.0),
+          child: DefaultTextStyle(
+            style: _computeTextStyle(rssi),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Text(rssi.toString()),
+                Text('dBm'),
+              ],
+            ),
+          ),
+        )
+            : Container(width: 0, height: 0),
+        device.isConnected
+            ? Icon(Icons.import_export)
+            : Container(width: 0, height: 0),
+        device.isBonded
+            ? Icon(Icons.link)
+            : Container(width: 0, height: 0),
+      ],
+    ),
+  );
+
+  static TextStyle _computeTextStyle(int rssi) {
+    /**/ if (rssi >= -35)
+      return TextStyle(color: Colors.greenAccent[700]);
+    else if (rssi >= -45)
+      return TextStyle(
+          color: Color.lerp(
+              Colors.greenAccent[700], Colors.lightGreen, -(rssi + 35) / 10));
+    else if (rssi >= -55)
+      return TextStyle(
+          color: Color.lerp(
+              Colors.lightGreen, Colors.lime[600], -(rssi + 45) / 10));
+    else if (rssi >= -65)
+      return TextStyle(
+          color: Color.lerp(Colors.lime[600], Colors.amber, -(rssi + 55) / 10));
+    else if (rssi >= -75)
+      return TextStyle(
+          color: Color.lerp(
+              Colors.amber, Colors.deepOrangeAccent, -(rssi + 65) / 10));
+    else if (rssi >= -85)
+      return TextStyle(
+          color: Color.lerp(
+              Colors.deepOrangeAccent, Colors.redAccent, -(rssi + 75) / 10));
+    else
+      /*code symetry*/
+      return TextStyle(color: Colors.redAccent);
+  }
+}
