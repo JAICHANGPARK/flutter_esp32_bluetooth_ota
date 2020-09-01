179aa6a8bddbd0a8ce03c506e01dfb3be3d0be25
JAICHANG.PARK <jaichang@angel-robotics.com>
2020-09-02 08:11:46 +0900

    Create spp_update_page.dart

---
+++ lib/spp_update_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+
+class SppUpdatePage extends StatefulWidget {
+  @override
+  _SppUpdatePageState createState() => _SppUpdatePageState();
+}
+
+class _SppUpdatePageState extends State<SppUpdatePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
