ce825924bcb5cfb0ad30c21d4a291f5e4f1c1508
JAICHANG.PARK <jaichang@angel-robotics.com>
2020-09-02 08:13:04 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,11 @@ import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_ap_updater/spp_update_page.dart';
 import 'package:flutter_blue/flutter_blue.dart';
 import 'package:permission_handler/permission_handler.dart';
+
+import 'ble_update_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -32,20 +35,20 @@ class SelectPage extends StatelessWidget {
       padding: const EdgeInsets.all(16),
       child: Column(
         children: [
-        ListTile(
-          title: Text("BLE UPDATE (TEST)"),
-        ),
           ListTile(
-            
+            title: Text("BLE UPDATE (TEST)"),
+            onTap: (){
+              Navigator.of(context).push(MaterialPageRoute(builder: (context) => MyHomePage(title: "ble update",)));
+            },
+          ),
+          ListTile(
             title: Text("BLUETOOTH UPDATE (TEST)"),
+            onTap: (){
+              Navigator.of(context).push(MaterialPageRoute(builder: (context) => SppUpdatePage()));
+            },
           )
         ],
       ),
     );
   }
 }
-
-
-
-
-
